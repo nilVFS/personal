eff8b726675a4c2eb3fe1ea4f368e252a9fb7e7d
Владислав Низьев <vladislavnizev@MacBook-Air-Vladislav.local>
2022-07-27 01:53:30 +0300

    animate photo wrapper

---
+++ src/scripts/main.js
@@ -0,0 +1,21 @@
+window.onwheel = e => {
+    if(e.deltaY >= 0){
+      document.querySelector("header").style.display = "none";
+      console.log('Scroll Down');
+    } else {
+        document.querySelector("header").style.display = "flex";
+    }
+}
+
+const myName = "Владислав Низьев";
+let i = 0;
+
+function typeWriter() {
+    if (i < myName.length) {
+      document.querySelector(".name").innerHTML += myName.charAt(i);
+      i++;
+      setTimeout(typeWriter, 100);
+    }
+  }
+
+document.body.addEventListener("load", typeWriter())
