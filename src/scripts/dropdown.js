5a2ead59b53d767ddf8b22f696fc40a5f9e9e963
Владислав Низьев <vladislavnizev@MacBook-Air-Vladislav.local>
2022-08-04 20:15:32 +0300

    add dropdown button animation

--- src/scripts/dropdown.js
+++ src/scripts/dropdown.js
@@ -1,18 +1,20 @@
 function showDrop() {
-    document.getElementById("dropdownList").classList.toggle("show");
+  document.getElementById("dropdownList").classList.toggle("show");
+  document.querySelector(".drop-btn").classList.add("transform")
 }
   
 window.onclick = function(e) {
-    if (!e.target.matches('.drop-btn')) {
-      const dropdowns = document.getElementsByClassName("dropdown-content");
-      let i;
-      for (i = 0; i < dropdowns.length; i++) {
-        let openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
+  if (!e.target.matches('.drop-btn')) {
+    const dropdowns = document.getElementsByClassName("dropdown-content");
+    let i;
+    for (i = 0; i < dropdowns.length; i++) {
+      let openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+        document.querySelector(".drop-btn").classList.remove("transform")
       }
     }
+  }
 }
 
 document.querySelector(".drop-btn").addEventListener("click", showDrop)
